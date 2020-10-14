9b0a2a04a76cdc60e880e04d37141af452a6d4d9
jainpriyanshi <jainpriyanshi2407@gmail.com>
2020-10-14 14:38:20 +0530

    first commit

---
+++ server/server.py
@@ -0,0 +1,22 @@
+import socket                
+
+s = socket.socket()          
+print("Socket successfully created")
+  
+port = 8000                                                                                                                                   
+HEADER_LENGTH = 10 
+hostname = socket.gethostname() 
+print(socket.gethostbyname(hostname))
+s.bind(('', port))         
+print(port) 
+
+s.listen(5)      
+print("socket is listening")            
+  
+while True: 
+   c, addr = s.accept()      
+   uh = c.recv(100)
+   print('Got connection from', addr)
+   print(uh)
+   #print(username)
+   
